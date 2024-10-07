111efab0a437fcff5c857986d68c5aac7a1d259a
Denskerz <de.errr18@gmail.com>
2024-10-07 11:44:21 -0700

    Update dag.py

--- dag.py
+++ dag.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
 
-dag = DAG('example_dag', start_date=datetime(2023, 1, 1))
+dag = DAG('example_dag', start_date=datetime(2024, 10, 10))
 
 start = DummyOperator(task_id='start', dag=dag)
 end = DummyOperator(task_id='end', dag=dag)
